9688a8554966c3f7b689ffd3ad6edf1df0d92a0a
Cadons <43477517+Cadons@users.noreply.github.com>
2024-11-26 17:19:32 +0100

    Update entrypoint.py

--- template/entrypoint.py
+++ template/entrypoint.py
@@ -5,26 +5,34 @@ def process_secrets():
     Process environment variables ending with _SECRET.
     For each, read the file at the path specified by the variable's value
     and export a new variable without the _SECRET suffix containing the secret content.
+    Writes these environment variables to a .env file.
     """
-    for key, value in os.environ.items():
-        if key.endswith("_SECRET"):
-            # Derive the new variable name by removing "_SECRET"
-            base_var_name = key[:-7]
+    with open("configure_secrets.sh", "w") as env_file:
+        for key, value in os.environ.items():
+            env_file.write("#!/bin/bash")
+            env_file.write("\n")
+            env_file.write("set -e")
+            env_file.write("\n")
+            env_file.write("echo 'configuring enviroment'\n")
+            if key.endswith("_SECRET"):
+                # Derive the new variable name by removing "_SECRET"
+                base_var_name = key[:-7]
 
-            # Check if the value points to a valid file
-            if os.path.isfile(value):
-                try:
-                    # Read the content of the secret file
-                    with open(value, "r") as secret_file:
-                        secret_value = secret_file.read().strip()
+                # Check if the value points to a valid file
+                if os.path.isfile(value):
+                    try:
+                        # Read the content of the secret file
+                        with open(value, "r") as secret_file:
+                            secret_value = secret_file.read().strip()
 
-                    # Export the new environment variable
-                    os.environ[base_var_name] = secret_value
-                    print(f"Exported {base_var_name} from secret {key} ({value})")
-                except Exception as e:
-                    print(f"Error reading secret file {value} for {key}: {e}")
-            else:
-                print(f"Warning: Secret file {value} not found for {key}")
+                        # Write the new variable to the .env file
+                        env_file.write(f'export {base_var_name}="{secret_value}";\n')
 
-# Process secrets
+                        print(f"Written {base_var_name} to configure_secrets.sh from secret {key} ({value})")
+                    except Exception as e:
+                        print(f"Error reading secret file {value} for {key}: {e}")
+                else:
+                    print(f"Warning: Secret file {value} not found for {key}")
+
+# Process secrets and write to .env file
 process_secrets()
